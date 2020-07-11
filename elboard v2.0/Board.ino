7b6d9218fca405241e3d6ea015cce02ca01e58c2
GargiMan <mage26gg@gmail.com>
2020-07-11 19:21:24 +0200

    Device share

--- elboard v2.0/Board.ino
+++ elboard v2.0/Board.ino
@@ -6,16 +6,19 @@
  * 
  */
 
-
 #include "config.h"     //library with settings
 
-#if (UPLOAD_DEVICE == 0)
-    //board compilation
+#if (UPLOAD_DEVICE == 0) //board compilation
+
     #include "VescUart.h"
-#elif (UPLOAD_DEVICE == 1)
-    //controller compilation
+
+    VescUart VESC;
+
+#elif (UPLOAD_DEVICE == 1) //controller compilation
+
     #include <driver/adc.h>
-#endif // UPLOAD_DEVICE
+
+#endif // UPLOAD_DEVICE (global)
 
 
 void setup() {
@@ -25,39 +28,52 @@ void setup() {
         DEBUG_PORT.begin(DEFAULT_PORT_SPEED);
     #endif // DEBUG_PORT
 
+    #if (UPLOAD_DEVICE == 0) //board compilation
 
-    //Vesc port check and setup
-    #ifdef VESC_CONTROL_PORT
+        //Vesc port check and setup
+        #ifdef VESC_CONTROL_PORT
 
-        //Vesc port open and set speed
-        #ifdef VESC_CONTROL_PORT_SPEED
-            VESC_CONTROL_PORT.begin(CONTROL_PORT_SPEED);
-        #else // VESC_CONTROL_PORT_SPEED
-            VESC_CONTROL_PORT.begin(DEFAULT_PORT_SPEED);
-        #endif // VESC_CONTROL_PORT_SPEED
-        VESC.setSerialPort(&VESC_CONTROL_PORT);
+            //Vesc port open and set speed
+            #ifdef VESC_CONTROL_PORT_SPEED
+                VESC_CONTROL_PORT.begin(CONTROL_PORT_SPEED);
+            #else // VESC_CONTROL_PORT_SPEED
+                VESC_CONTROL_PORT.begin(DEFAULT_PORT_SPEED);
+            #endif // VESC_CONTROL_PORT_SPEED
+            VESC.setSerialPort(&VESC_CONTROL_PORT);
 
-        #ifdef VESC_DEBUG
-            VESC.setDebugPort(&VESC_DEBUG_PORT);
-        #endif // VESC_DEBUG
+            #ifdef VESC_DEBUG
+                VESC.setDebugPort(&VESC_DEBUG_PORT);
+            #endif // VESC_DEBUG
 
-    #endif // VESC_CONTROL_PORT
+        #endif // VESC_CONTROL_PORT
 
-    //Front light setup
-    #ifdef FLIGHT_PIN
-        ledcSetup(1, 5000, 8);
-        ledcAttachPin(FLIGHT_PIN, 1);
-    #endif
+        //Front light setup
+        #ifdef FLIGHT_PIN
+            ledcSetup(1, 5000, 8);
+            ledcAttachPin(FLIGHT_PIN, 1);
+        #endif
 
-    //Rear light setup
-    #ifdef RLIGHT_PIN
-        ledcSetup(2, 5000, 8);
-        ledcAttachPin(RLIGHT_PIN, 2);
-    #endif
+        //Rear light setup
+        #ifdef RLIGHT_PIN
+            ledcSetup(2, 5000, 8);
+            ledcAttachPin(RLIGHT_PIN, 2);
+        #endif
+
+    #elif (UPLOAD_DEVICE == 1) //controller compilation
+
+
+
+    #endif // UPLOAD_DEVICE (setup)
 
 }
 
 
 void loop() {
+
+    #if (UPLOAD_DEVICE == 0) //board compilation
+
+    #elif (UPLOAD_DEVICE == 1) //controller compilation
+
+    #endif // UPLOAD_DEVICE (loop)
 
 }
--- elboard v2.0/config.h
+++ elboard v2.0/config.h
@@ -5,13 +5,16 @@
 #define UPLOAD_DEVICE 0                     //Device where code will be uploaded, 0 = board / 1 = controller
 
 
+//---------------------------------//
+//----------BOARD-SETTINGS---------//
+//---------------------------------//
+
 //port and vesc settings
 #define DEFAULT_PORT_SPEED 115200           //Default comm and vesc speed , need to be defined for any communication
 //#define DEBUG_PORT Serial1                  //Debug port , no debug if comented
-#define VESC Vesc                           //Vesc prefix
 #define VESC_CONTROL_PORT Serial2           //Vesc control , default port speed or custom
 //#define VESC_CONTROL_PORT_SPEED 115200      //Vesc control speed
-//#define VESC_DEBUG_PORT Serial1             //Vesc debug , default port speed , no vesc debug if comented
+//#define VESC_DEBUG_PORT Serial1             //Vesc debug print , default port speed , no vesc debug if comented
 
 
 //lights settings
@@ -33,5 +36,22 @@
 #define JOY_HOLD_CHANGE_L 0.03      //maximálna zmena pri držaní hold , mod 1/2 , 1 - 100
 #define JOY_BRAKE_POWER_H 20        //maximálna hodnota brzdiaceho prúdu, mod 3/4
 #define JOY_BRAKE_POWER_L 10        //maximálna hodnota brzdiaceho prúdu, mod 1/2
+
+//---------------------------------//
+//-------CONTROLLER-SETTINGS-------//
+//---------------------------------//
+
+//sleep settings
+#define SLEEP_PIN 0x000000004       //hex - 2 wake up pin
+#define SLEEP_AFK_TIME 30000        //millis without any change , deepsleep
+#define SLEEP_DEAD 50               //odchýlka od deadzone + aj - , zvykne občas skočiť hodnota 
+
+//controls settings
+#define POWER_PIN 32                //analog, throtle / brake value
+#define HOLD_PIN 2                  //digital, hold speed control
+#define MODE_PIN 13                 //digital, mode switch
+#define LIGHT_PIN 16                //digital, light control
+//#define LIGHT_PIN 17                //digital, rear light control
+#define ROTATE_PIN 4                //digital, drive rotation control
 
 #endif //CONFIG_H
