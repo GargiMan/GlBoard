40b6449828cc1d445833bd96da0393b568260dab
Marek Gergel <mage26gg@gmail.com>
2023-12-15 00:02:11 +0100

    Fixed C++ compilation issues

--- src/crc8.h
+++ src/crc8.h
@@ -8,6 +8,10 @@
 #ifndef CRC8_H_
 #define CRC8_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /**
  * Calculate CRC8 checksum
  * @param buf buffer
@@ -15,5 +19,9 @@
  * @return checksum
  */
 unsigned char crc8(unsigned char *buf, unsigned int len);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // CRC8_H_
