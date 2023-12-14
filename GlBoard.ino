eb7e773108668672b500651929acc0809f3e55ca
Marek Gergel <mage26gg@gmail.com>
2023-12-14 01:24:08 +0100

    Added config data storing, added light control

--- GlBoard.ino
+++ GlBoard.ino
@@ -3,6 +3,7 @@
  *
  * Created on: 13 jul 2020
  *     Author: Marek Gergel
+ *    Version: 2.0
  *
  * File 'config.h' can be modified with custom configuration.
  */
@@ -15,20 +16,24 @@
 #include "src/VescUart.h"
 #include "src/crc8.h"
 #include <BluetoothSerial.h>
+#include <Preferences.h>
 
 VescUart VESC;
 BluetoothSerial BT_PORT;
+Preferences preferences;
 
 bool hasClient = false;
+int maxLightPower = pow(2, PWM_RESOLUTION) - 1;
 
 void debug_message(const char *fmt, ...);
 void read_rest_of_buffer();
 void verify_architecture();
 void send_metrics_data();
-void parse_recieved_data();
+void parse_received_data();
+bool parse_config_data(uint8_t* payload, uint8_t payloadLen);
+void update_lights();
 
 void setup() {
-
     // debug
 #if defined(DEBUG) || defined(VESC_DEBUG)
     DEBUG_PORT.begin(PORT_SPEED);
@@ -37,6 +42,30 @@ void setup() {
 #endif // VESC_DEBUG
 #endif // DEBUG || VESC_DEBUG
 
+    verify_architecture();
+
+    debug_message("Project version: %s", PROJECT_VERSION);
+
+    // preferences
+    preferences.begin(PROJECT_NAME, RO_MODE);
+    if (preferences.isKey(KEY_PROJECT_VERSION) && preferences.getString(KEY_PROJECT_VERSION) == PROJECT_VERSION) {
+        debug_message("Preferences loaded");
+    } else { // preferences not found or project version changed
+        preferences.end();
+        preferences.begin(PROJECT_NAME, RW_MODE);
+        preferences.clear();
+        preferences.putString(KEY_PROJECT_VERSION, PROJECT_VERSION);
+        preferences.putInt(KEY_MAX_THROTTLE_POWER, MAX_THROTTLE_POWER);
+        preferences.putInt(KEY_MAX_BRAKE_POWER, MAX_BRAKE_POWER);
+        preferences.putBool(KEY_FRONT_LIGHT_ENABLED, false);
+        preferences.putFloat(KEY_FRONT_LIGHT_POWER, FRONT_LIGHT_POWER);
+        preferences.putBool(KEY_REAR_LIGHT_ENABLED, false);
+        preferences.putFloat(KEY_REAR_LIGHT_POWER, REAR_LIGHT_POWER);
+        preferences.end();
+        preferences.begin(PROJECT_NAME, RO_MODE);
+        debug_message("Preferences recreated");
+    }
+
     // vesc
     VESC_PORT.begin(PORT_SPEED);
     VESC.setSerialPort(&VESC_PORT);
@@ -44,27 +73,37 @@ void setup() {
 
     // bluetooth
     BT_PORT.begin(BT_DEVICE); // start Bluetooth with device name
-    debug_message("Bluetooth ready: %s", BT_DEVICE);
+    debug_message("Bluetooth ready (%s)", BT_DEVICE);
 
     // pin setup
-    pinMode(LED_PIN_ONBOARD, OUTPUT);
+    pinMode(LED_ONBOARD_PIN, OUTPUT);
+    ledcSetup(FRONT_LIGHT_CHANNEL, PWM_FREQUENCY, PWM_RESOLUTION);
+    ledcAttachPin(FRONT_LIGHT_PIN, FRONT_LIGHT_CHANNEL);
+    ledcSetup(REAR_LIGHT_CHANNEL, PWM_FREQUENCY, PWM_RESOLUTION);
+    ledcAttachPin(REAR_LIGHT_PIN, REAR_LIGHT_CHANNEL);
+    debug_message("GPIO ready");
 }
 
 void loop() {
-    verify_architecture();
 
-    // check if client is connected
+    // update bluetooth connection status
     if (hasClient != BT_PORT.hasClient()) {
         hasClient = BT_PORT.hasClient();
         debug_message("Client %s", hasClient ? "connected" : "disconnected");
-        digitalWrite(LED_PIN_ONBOARD, BT_PORT.hasClient() ? HIGH : LOW);
+        digitalWrite(LED_ONBOARD_PIN, BT_PORT.hasClient() ? HIGH : LOW);
     }
 
-    // send metrics data
-    send_metrics_data();
+    // Check if client is connected
+    if (BT_PORT.hasClient()){
+        // send metrics data
+        send_metrics_data();
 
-    // recieve
-    parse_recieved_data();
+        // receive
+        parse_recieved_data();
+    }
+
+    // lights
+    update_lights();
 }
 
 /**
@@ -108,9 +147,6 @@ void verify_architecture() {
  * Send data from VESC to the client.
  */
 void send_metrics_data() {
-    // Check if client is connected
-    if (!BT_PORT.hasClient()) return;
-
     static unsigned long lastTime = 0;
     if (millis() - lastTime < METRICS_UPDATE_DELAY) return;
 
@@ -162,7 +198,7 @@ void send_metrics_data() {
     }
 }
 
-void parse_recieved_data() {
+void parse_received_data() {
     static unsigned long lastTimeValidControlReceived = 0;
     static bool resetCurrent = false;
 
@@ -206,7 +242,7 @@ void parse_recieved_data() {
                 if (IS_REQUEST(flags)) {
                     if (payloadLen != 2) {
                         read_rest_of_buffer();
-                        debug_message("Wrong message payload length");
+                        debug_message("Wrong control payload");
                         delete[] packet;
                         return;
                     }
@@ -234,7 +270,15 @@ void parse_recieved_data() {
                 }
                 break;
             case CONFIG_FLAG:
-                debug_message("Config message recieved");
+                if (IS_REQUEST(flags)) {
+                    bool success = parse_config_data(payload, payloadLen);
+                    if (!success) {
+                        read_rest_of_buffer();
+                        debug_message("Wrong config payload");
+                        delete[] packet;
+                        return;
+                    }
+                }
                 break;
             default:
                 debug_message("Wrong message command");
@@ -250,5 +294,76 @@ void parse_recieved_data() {
                 VESC.setCurrent(0);
             }
         }
+    }
+}
+
+bool parse_config_data(uint8_t* payload, uint8_t payloadLen) {
+    int payloadIndex = 0;
+    bool error = false;
+
+    // check if payload is not empty and reopens preferences for writing
+    if (payloadLen != 0) {
+        preferences.end();
+        preferences.begin(PROJECT_NAME, RW_MODE);
+
+        while (payloadIndex < payloadLen && !error) {
+            uint8_t config = payload[payloadIndex++];
+            switch (config) {
+                case CFG_FRONT_LIGHT:
+                    preferences.putBool(KEY_FRONT_LIGHT_ENABLED, !preferences.getBool(KEY_FRONT_LIGHT_ENABLED));
+                    debug_message("Front light %s", preferences.getBool(KEY_FRONT_LIGHT_ENABLED) ? "enabled" : "disabled");
+                    continue;
+                case CFG_REAR_LIGHT:
+                    preferences.putBool(KEY_REAR_LIGHT_ENABLED, !preferences.getBool(KEY_REAR_LIGHT_ENABLED));
+                    debug_message("Rear light %s", preferences.getBool(KEY_REAR_LIGHT_ENABLED) ? "enabled" : "disabled");
+                    continue;
+                case CFG_MAX_THROTTLE_POWER:
+                    if (payloadIndex + 1 > payloadLen) {
+                        error = true;
+                        break;
+                    }
+                    // check if value is different from current value, reduce number of writes to flash memory
+                    if (payload[payloadIndex] != preferences.getInt(KEY_MAX_THROTTLE_POWER)) {
+                        preferences.putInt(KEY_MAX_THROTTLE_POWER, payload[payloadIndex]);
+                        debug_message("Max throttle power set to %d", preferences.getInt(KEY_MAX_THROTTLE_POWER));
+                    }
+                    payloadIndex++;
+                    continue;
+                case CFG_MAX_BRAKE_POWER:
+                    if (payloadIndex + 1 > payloadLen) {
+                        error = true;
+                        break;
+                    }
+                    // check if value is different from current value, reduce number of writes to flash memory
+                    if (payload[payloadIndex] != preferences.getInt(KEY_MAX_BRAKE_POWER)) {
+                        preferences.putInt(KEY_MAX_BRAKE_POWER, payload[payloadIndex]);
+                        debug_message("Max brake power set to %d", preferences.getInt(KEY_MAX_BRAKE_POWER));
+                    }
+                    payloadIndex++;
+                    continue;
+                default:
+                    error = true;
+                    break;
+            }
+        }
+
+        // close preferences for writing and reopen for reading
+        preferences.end();
+        preferences.begin(PROJECT_NAME, RO_MODE);
+    }
+
+    return !error;
+}
+
+void update_lights() {
+    if (preferences.getBool(KEY_FRONT_LIGHT_ENABLED)) {
+        ledcWrite(FRONT_LIGHT_CHANNEL, maxLightPower * preferences.getFloat(KEY_FRONT_LIGHT_POWER));
+    } else {
+        ledcWrite(FRONT_LIGHT_CHANNEL, 0);
+    }
+    if (preferences.getBool(KEY_REAR_LIGHT_ENABLED)) {
+        ledcWrite(REAR_LIGHT_CHANNEL, maxLightPower * preferences.getFloat(KEY_REAR_LIGHT_POWER));
+    } else {
+        ledcWrite(REAR_LIGHT_CHANNEL, 0);
     }
 }
